de2bbf182cabcdfa19922df54905aee1fa9fc683
Lauryn <laurynpearson@gmail.com>
2019-06-23 10:11:37 -0500

    initial commit

---
+++ app/controllers/api/example_pages_controller.rb
@@ -0,0 +1,5 @@
+class Api::ExamplePagesController < ApplicationController
+  # def the_good_morning_method
+  #   # reponse
+  #   render json: {message: "good morning"}
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# Rails.application.routes.draw do
+#   # EXAMPLE HTML ROUTE
+#   # get "/photos" => "photos#index"
+#   namespace :api do
+#     #    route(chrome)      controller          method
+#     # goes in controller
+#     get '/good_morning' => 'example_pages#the_good_morning_method'
+#   # EXAMPLE JSON ROUTE WITH API NAMESPACE
+#   # namespace :api do
+#   #   get "/photos" => "photos#index"
+#   # end
+end
